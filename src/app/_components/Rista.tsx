eb899c57715cb6e7620004a07c6c58087a828f58
Omegz <omarmajed2000@hotmail.com>
2024-05-23 18:36:37 +0200

    success div

--- src/app/_components/Rista.tsx
+++ src/app/_components/Rista.tsx
@@ -22,6 +22,13 @@ const SliderContainer = styled.div`
   position: relative;
 `;
 
+const Line = styled.div`
+  height: 2px;
+  background: linear-gradient(to right, transparent 50%, #223049 50%);
+  background-size:
+    16px 2px,
+    100% 2px;
+`;
 const SliderInput = styled.input`
   width: 100%;
   appearance: none;
@@ -165,41 +172,71 @@ const Rista: React.FC = () => {
   });
 
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-red-500 p-2">
-      <div
-        className={`m-7 w-full border-8 ${isPaid ? "border-green-400" : "border-yellow-400"} bg-white`}
-      >
-        <div
-          className={`flex items-center justify-center ${isPaid ? "visible" : "invisible"}`}
-        >
-          hellow world
+    // <div className="flex h-screen flex-col items-center justify-center bg-red-500 p-2">
+    //   <div
+    //     className={`m-7 w-full border-8 ${isPaid ? "border-green-400" : "border-yellow-400"} bg-white`}
+    //   >
+    //     <div
+    //       className={`flex items-center justify-center ${isPaid ? "visible" : "invisible"}`}
+    //     >
+    //       hellow world
+    //     </div>
+    //     <div className="mt-24 flex">
+    //       <div className="mb-12 ml-12 flex w-1/2 flex-col justify-center">
+    //         <h1 className="text">LOCATION</h1>
+    //         <h1>Tilda og Karl</h1>
+    //         <p>{formattedTime}</p>
+    //         <p>{formattedDate}</p>
+    //       </div>
+    //     </div>
+    //     <div className="">
+    //       <div className="mb-36 h-12 bg-gray-500">
+    //         <h1 className="ml-12">Islatte</h1>
+    //       </div>
+    //     </div>
+    //     <SliderWrapper className="bg-white">
+    //       <SliderLabel>Slide to Pay</SliderLabel>
+    //       <SliderContainer>
+    //         <SliderInput
+    //           type="range"
+    //           ref={inputRangeRef}
+    //           value={value}
+    //           onChange={handleChange}
+    //           onMouseUp={handleMouseUp}
+    //           onTouchEnd={handleMouseUp}
+    //         />
+    //       </SliderContainer>
+    //     </SliderWrapper>
+    //   </div>
+    // </div>
+
+    <div className="h-screen bg-blue-500">
+      <div className="items-cetner ml-4 mr-4 mt-12 flex flex-col justify-center bg-green-500">
+        <div className=" flex flex-col items-center justify-center">svg</div>
+        <div className=" flex flex-col items-center justify-center">
+          Receipt
         </div>
-        <div className="mt-24 flex">
-          <div className="mb-12 ml-12 flex w-1/2 flex-col justify-center">
-            <h1 className="text">LOCATION</h1>
-            <h1>Tilda og Karl</h1>
-            <p>{formattedTime}</p>
-            <p>{formattedDate}</p>
+      </div>
+
+      <div className="ml-4  mr-4  flex flex-col items-center justify-center bg-red-500">
+        <div className="ml-5 mr-5 w-full border-8 border-green-400">
+          <div className="items-cetner mt-12 flex justify-center ">0 Kr.</div>
+          <Line className="mb-3 mt-12" />
+          <div className=" flex">
+            <div className=" ml-12 flex w-1/2 flex-col justify-center">
+              <h1 className="text">LOCATION</h1>
+              <h1>Tilda og Karl</h1>
+              <p>{formattedTime}</p>
+              <p>{formattedDate}</p>
+            </div>
+          </div>
+          <Line className=" mb-12 mt-3" />
+          <div className="">
+            <div className="mb-36 h-12 bg-gray-500">
+              <h1 className="ml-12">Islatte</h1>
+            </div>
           </div>
         </div>
-        <div className="">
-          <div className="mb-36 h-12 bg-gray-500">
-            <h1 className="ml-12">Islatte</h1>
-          </div>
-        </div>
-        <SliderWrapper className="bg-white">
-          <SliderLabel>Slide to Pay</SliderLabel>
-          <SliderContainer>
-            <SliderInput
-              type="range"
-              ref={inputRangeRef}
-              value={value}
-              onChange={handleChange}
-              onMouseUp={handleMouseUp}
-              onTouchEnd={handleMouseUp}
-            />
-          </SliderContainer>
-        </SliderWrapper>
       </div>
     </div>
   );
